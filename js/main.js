e507b76efde632d772a1bdc248d0bcabadff3f43
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-10-20 16:21:46 +0200

    divisione del problema

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//Dichiarazioni Variabili
+//Chiedere l'eta' del passeggero
+//Chiedere il numero di km da percorrere
+//Prezzo del biglietto al km
+//Calcola sconto in base all'eta'
+//Applica sconto in base all'eta'
